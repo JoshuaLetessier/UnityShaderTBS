c310a63bad860873b548ed25e63d7f3daa0632f1
deadlysinss111 <vgautier@gaming.tech>
2024-11-26 17:44:28 +0100

    sequencer

---
+++ Assets/Script/AbilityMapper.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class AbilityMapper
+{
+    //static Dictionary<string, Competence> map = new()
+    //{
+    //    {"dummy", new DummyAction()},
+    //};
+}
