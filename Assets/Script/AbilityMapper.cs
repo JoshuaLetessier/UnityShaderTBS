9694effa04ccd198a36e0f168c7e099fcdba54b3
Gautier Valentin <vgautier@gaming.tech>
2024-11-27 08:49:37 +0100

    abilities fix

--- Assets/Script/AbilityMapper.cs
+++ Assets/Script/AbilityMapper.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 static class AbilityMapper
 {
-    public static Dictionary<string, Func<PlayerTeam, CombatManager, bool>> map = new()
+    public static Dictionary<string, Func<Competence>> map = new()
     {
-        {"dummy", (PlayerTeam playerTeam, CombatManager combatManager)=>{new DummyAction(playerTeam, combatManager); return true; } },
+        {"dummy", ()=>{return new CompetenceTest(); } },
     };
 }
